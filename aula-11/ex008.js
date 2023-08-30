c92133e7f493f09221ee98503216cc407cde48c6
antoniocvjr <antoniovieira783@gmail.com>
2023-08-30 16:39:12 -0300

    Tudo que aprendi até agora

---
+++ aula-11/ex008.js
@@ -0,0 +1,23 @@
+var vel = 57
+
+
+console.log(`A velocidade do seu carro é ${vel} KM por hora`)
+
+
+if(vel > 60){
+
+    console.log('Você ultrapassou a velocidade permitida - MULTADO')
+
+
+}
+console.log('Dirija sempre usando cinto de segurança')
+
+
+
+
+
+
+
+// console.log('O Console está funcionando')
+
+
---
+++ aula-11/ex009.js
@@ -0,0 +1,12 @@
+var pais = 'Brasil'
+
+if (pais == 'Brasil' ){
+
+    console.log('Brasileiro(a)')
+
+} else {
+
+
+    console.log('Estrangeiro(a)')
+
+}
